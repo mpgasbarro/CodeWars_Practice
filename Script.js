0e400c233a38327def2cf35f9f6a99df7ca6704d
Gaspy <mpgasbarro@gmail.com>
2020-11-02 14:33:23 -0500

    have the problem solved but since count is declared globally, the codewars isn't accepting the solution. Working on whatelse I can do

--- Script.js
+++ Script.js
@@ -904,17 +904,51 @@
 
 // ***** SOLUTION *****
 
-function digital_root(n) {
-	let sum = 0;
-	let str = n.toString().split('');
-	if (str.length <= 1) {
-		return n;
+// function digital_root(n) {
+// 	let sum = 0;
+// 	let str = n.toString().split('');
+// 	if (str.length <= 1) {
+// 		return n;
+// 	} else {
+// 		for (let i = 0; i < str.length; i++) {
+// 			sum += parseInt(str[i]);
+// 		}
+// 	}
+// 	return digital_root(sum);
+// }
+
+// console.log(digital_root(456));
+
+// CODEWARS PRACTICE - 11/02/20
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example:
+// persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+//                        // and 4 has only one digit
+
+//  persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
+//                         // 1*2*6 = 12, and finally 1*2 = 2
+
+//  persistence(4) === 0 // because 4 is already a one-digit number
+
+let count = 0;
+
+function persistence(num) {
+	let sum = 1;
+	let newNum = sum;
+	numStr = num.toString().split('');
+
+	if (numStr.length <= 1) {
+		return count;
+		count = 0;
 	} else {
-		for (let i = 0; i < str.length; i++) {
-			sum += parseInt(str[i]);
+		for (let i = 0; i < numStr.length; i++) {
+			sum *= parseInt(numStr[i]);
 		}
+		count++;
+		return persistence(sum);
 	}
-	return digital_root(sum);
+	return count;
 }
 
-console.log(digital_root(456));
+persistence(25);
