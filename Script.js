639144c4afa8d7ba0f1ce719e32fb393424f35bd
Gaspy <mpgasbarro@gmail.com>
2020-11-10 16:29:06 -0500

    finished solving the codewar problem from yesterday. Starting a new problem today

--- Script.js
+++ Script.js
@@ -1077,13 +1077,44 @@
 // pigIt('Hello world !');     // elloHay orldway !
 
 // ***** SOLUTION *****
-function pigIt(str) {
-	let newArr = str.split(' ');
-	let valOfInd = '';
-	for (let i = 0; i < newArr.length; i++) {
-		newArr[i].charAt(0);
-	}
-	console.log(newArr);
-}
+// function pigIt(str) {
+// 	let newArr = str.split(' ');
+// 	let valOfInd = '';
+// 	for (let i = 0; i < newArr.length; i++) {
+// 		newArr[i].charAt(0);
+// 	}
+// 	console.log(newArr);
+// }
 
-pigIt('Pig latin is cool');
+// pigIt('Pig latin is cool');
+
+// ____________________________________________________________________________
+// CODEWARS PRACTICE - 11/10/20
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+// Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+//  ***** SOLUTION *****
+// function isValidWalk(walk) {
+// 	let count = 0
+// 	if(walk.length !== 10){
+// 		return false
+// 	} else {
+// 	for (let i = 0; i < walk.length; i++) {
+// 		if( walk[i] == "n"){
+// 			count++
+// 		} else if (walk[i] == "s"){
+// 			count--
+// 		} else if (walk[i] == "e"){
+// 			count++
+// 		} else {
+// 			count--
+// 		}
+// 	}
+// }
+
+// 	return count === 0 ? true: false
+
+// }
+
+// console.log(isValidWalk(['e', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'w']))
