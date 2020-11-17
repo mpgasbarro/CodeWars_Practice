aa9f349e0a17669ffb4c74fddf84c5504bb54d74
Gaspy <mpgasbarro@gmail.com>
2020-11-17 08:53:12 -0500

    Solved Codewars Problem-11/17, kata-6

--- Script.js
+++ Script.js
@@ -1172,4 +1172,27 @@
 // };
 
 // squareDigits(9119);
-console.log('hello hello');
+// console.log('hello hello');
+
+// _________________________________________________________________
+// CODEWARS - 11/17/20
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+function arrayDiff(a, b) {
+	let newArr = [];
+	for (let i = 0; i < a.length; i++) {
+		if (b.indexOf(a[i]) === -1) {
+			newArr.push(a[i]);
+		}
+	}
+	return newArr;
+}
+
+arrayDiff([1, 2], [1]);
