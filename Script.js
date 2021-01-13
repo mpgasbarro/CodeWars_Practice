5b8fc2211704fa934f1d906b837763633a56cbc5
Gaspy <mpgasbarro@gmail.com>
2021-01-13 17:18:18 -0500

    2d array problem partially solved. Have two test cases that are not working

--- Script.js
+++ Script.js
@@ -1547,22 +1547,64 @@
 // 	}
 // }
 
-function threeSum(arr, target) {
-	for (let i = 0; i < arr.length; i++) {
-		var left = i + 1;
-		var right = arr.length - 1;
-		while (left < right) {
-			var curr = arr[i] + arr[left] + arr[right];
-			if (curr < target) {
-				left++;
-			} else if (curr > target) {
-				right--;
-			} else {
-				return [arr[i], arr[left], arr[right]];
+// function threeSum(arr, target) {
+// 	for (let i = 0; i < arr.length; i++) {
+// 		var left = i + 1;
+// 		var right = arr.length - 1;
+// 		while (left < right) {
+// 			var curr = arr[i] + arr[left] + arr[right];
+// 			if (curr < target) {
+// 				left++;
+// 			} else if (curr > target) {
+// 				right--;
+// 			} else {
+// 				return [arr[i], arr[left], arr[right]];
+// 			}
+// 		}
+// 	}
+// 	return [-1, -1, -1];
+// }
+
+// console.log(threeSum([1, 2, 4, 5, 12], 19));
+
+//
+// function squared(arr){
+// 	let newArr = arr.sort(function (a, b) {
+// 		return (a*a)-(b*b);
+// 	});
+// 	console.log(newArr);
+// }
+
+// squared([-4, -6, -2, 1, 4, 5])
+
+//
+
+function hourglassSum(arr) {
+	let total = 0;
+
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			let sum =
+				arr[i][j] +
+				arr[i][j + 1] +
+				arr[i][j + 2] +
+				arr[i + 1][j + 1] +
+				arr[i + 2][j] +
+				arr[i + 2][j + 1] +
+				arr[i + 2][j + 2];
+			if (sum > total) {
+				total = sum;
 			}
 		}
 	}
-	return [-1, -1, -1];
+	console.log(total);
 }
 
-console.log(threeSum([1, 2, 4, 5, 12], 19));
+hourglassSum([
+	[1, 1, 1, 0, 0, 0],
+	[0, 1, 0, 0, 0, 0],
+	[1, 1, 1, 0, 0, 0],
+	[0, 0, 2, 4, 4, 0],
+	[0, 0, 0, 2, 0, 0],
+	[0, 0, 1, 2, 4, 0],
+]);
