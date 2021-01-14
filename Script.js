480ba05ec75eeed6f1141f979ab8890b74d34308
Gaspy <mpgasbarro@gmail.com>
2021-01-13 19:56:37 -0500

    solved problem again but with for loop

--- Script.js
+++ Script.js
@@ -1579,32 +1579,52 @@
 
 //
 
-function hourglassSum(arr) {
-	let total = ;
+// function hourglassSum(arr) {
+// 	let total = ;
 
-	for (let i = 0; i < 4; i++) {
-		for (let j = 0; j < 4; j++) {
-			let sum =
-				arr[i][j] +
-				arr[i][j + 1] +
-				arr[i][j + 2] +
-				arr[i + 1][j + 1] +
-				arr[i + 2][j] +
-				arr[i + 2][j + 1] +
-				arr[i + 2][j + 2];
-			if (sum > total) {
-				total = sum;
-			}
+// 	for (let i = 0; i < 4; i++) {
+// 		for (let j = 0; j < 4; j++) {
+// 			let sum =
+// 				arr[i][j] +
+// 				arr[i][j + 1] +
+// 				arr[i][j + 2] +
+// 				arr[i + 1][j + 1] +
+// 				arr[i + 2][j] +
+// 				arr[i + 2][j + 1] +
+// 				arr[i + 2][j + 2];
+// 			if (sum > total) {
+// 				total = sum;
+// 			}
+// 		}
+// 	}
+// 	console.log(total);
+// }
+
+// hourglassSum([
+// 	[1, 1, 1, 0, 0, 0],
+// 	[0, 1, 0, 0, 0, 0],
+// 	[1, 1, 1, 0, 0, 0],
+// 	[0, 0, 2, 4, 4, 0],
+// 	[0, 0, 0, 2, 0, 0],
+// 	[0, 0, 1, 2, 4, 0],
+// ]);
+
+// _________________________________________
+// FIND TWO NUMBERS IN A SORTED ARRAY THAT SUM TO A TARGET. PRACTICING AGAIN
+
+function findSum(arr, target) {
+	let left = 0;
+	let right = arr.length - 1;
+
+	while (left < right) {
+		if (arr[left] + arr[right] < target) {
+			left++;
+		} else if (arr[left] + arr[right] > target) {
+			right--;
+		} else {
+			return [arr[left], arr[right]];
 		}
 	}
-	console.log(total);
 }
 
-hourglassSum([
-	[1, 1, 1, 0, 0, 0],
-	[0, 1, 0, 0, 0, 0],
-	[1, 1, 1, 0, 0, 0],
-	[0, 0, 2, 4, 4, 0],
-	[0, 0, 0, 2, 0, 0],
-	[0, 0, 1, 2, 4, 0],
-]);
+console.log(findSum([-1, 1, 2, 3, 5], 5));
