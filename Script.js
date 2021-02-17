8cf90e330964836c70942911ad4d80b207d4a073
Gaspy <mpgasbarro@gmail.com>
2021-02-17 10:15:14 -0500

    finally solved left array

--- Script.js
+++ Script.js
@@ -2020,22 +2020,39 @@
 // "("               =>  false
 // "(())((()())())"  =>  true
 
-function validParentheses(parens) {
-	let openCount = 0;
-	let closeCount = 0;
-	let newArr = parens.split('');
-	for (let i = 0; i < newArr.length; i++) {
-		if (newArr[i] === '(' && openCount >= closeCount) {
-			openCount++;
-		} else {
-			closeCount++;
-		}
+// function validParentheses(parens) {
+// 	let openCount = 0;
+// 	let closeCount = 0;
+// 	let newArr = parens.split('');
+// 	for (let i = 0; i < newArr.length; i++) {
+// 		if (newArr[i] === '(' && openCount >= closeCount) {
+// 			openCount++;
+// 		} else {
+// 			closeCount++;
+// 		}
+// 	}
+// 	if (openCount === closeCount) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }
+
+// validParentheses(')(()))');
+
+// ___________________________________________________________
+// HACKERRANK - 2/17 - Left Rotation
+// A left rotation operation on an array shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become . Note that the lowest index item moves to the highest index in a rotation. This is called a circular array.
+
+// Given an array  of  integers and a number, , perform  left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
+
+function rotLeft(a, d) {
+	let newArr = [];
+	for (let i = 0; i < d; i++) {
+		let first = a.shift();
+		a.push(first);
 	}
-	if (openCount === closeCount) {
-		return true;
-	} else {
-		return false;
-	}
+	return a;
 }
 
-validParentheses(')(()))');
+rotLeft([1, 2, 3, 4, 5], 4);
