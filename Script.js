953f5d7c59ddf8bcc3d05f40fd14b121b64ce968
Gaspy <mpgasbarro@gmail.com>
2021-02-18 09:17:54 -0500

    solved easy hackerrank

--- Script.js
+++ Script.js
@@ -2056,3 +2056,28 @@
 // }
 
 // rotLeft([1, 2, 3, 4, 5], 4);
+
+// -_________________________________________________________
+// HackerRank problem - 4/18 -
+// You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+// Example
+
+// The maximum height candles are  units high. There are  of them, so return .
+
+function birthdayCakeCandles(candles) {
+	let sortedCandles = candles.sort(function (a, b) {
+		return a - b;
+	});
+	let lastNumber = sortedCandles[candles.length - 1];
+	let count = 0;
+
+	for (let i = 0; i < candles.length; i++) {
+		if (candles[i] === lastNumber) {
+			count++;
+		}
+	}
+	return count;
+}
+
+birthdayCakeCandles([4, 4, 1, 3]);
